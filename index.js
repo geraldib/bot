6e23f27386e8c4158fb5421bee4217cee0227af7
Gerald Ibra <gerald.ibra@crispybacon.al>
2020-12-04 12:27:47 +0100

    aaaa

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 const {google} = require('googleapis');
 
 const app = express()
@@ -18,21 +18,21 @@ app.post('/', (req, res) => {
   if (req.body.type === 'ADDED_TO_SPACE' && req.body.space.type === 'ROOM') {
     text = `Thanks for adding me to ${req.body.space.displayName}`;
 
-    // var fileMetadata = {
-    //     'name': req.body.space.displayName,
-    //     'mimeType': 'application/vnd.google-apps.folder'
-    //   };
-    //   drive.files.create({
-    //     resource: fileMetadata,
-    //     fields: 'id'
-    //   }, function (err, file) {
-    //     if (err) {
-    //       // Handle error
-    //         text = `Folder was not created!!!`;
-    //     } else {
-    //         text = `Folder: "${req.body.space.displayName}" with ID: "${file}" was created!`;
-    //     }
-    //   });
+    var fileMetadata = {
+        'name': req.body.space.displayName,
+        'mimeType': 'application/vnd.google-apps.folder'
+      };
+      drive.files.create({
+        resource: fileMetadata,
+        fields: 'id'
+      }, function (err, file) {
+        if (err) {
+          // Handle error
+            text = `Folder was not created!!!`;
+        } else {
+            text = `Folder: "${req.body.space.displayName}" with ID: "${file}" was created!`;
+        }
+      });
 
   // Case 2: When BOT was added to a DM
   } else if (req.body.type === 'ADDED_TO_SPACE' &&
