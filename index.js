c9debaeb5f6ba022297437e2a4ce0a059d727c49
Gerald Ibra <gerald.ibra@crispybacon.al>
2020-12-04 12:17:37 +0100

    fixed bug

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const PORT = process.env.PORT || 9000;
-const {google} = require('googleapis')
+const PORT = process.env.PORT || 5000;
 
 const app = express()
     .use(express.urlencoded({extended: false}))
@@ -10,7 +9,6 @@ app.get('/', (req, res) => {
     res.send('Hi');
 });
 
-
 app.post('/', (req, res) => {
 
   let text = '';
@@ -19,15 +17,21 @@ app.post('/', (req, res) => {
   if (req.body.type === 'ADDED_TO_SPACE' && req.body.space.type === 'ROOM') {
     text = `Thanks for adding me to ${req.body.space.displayName}`;
 
-    let fileMetadata = {
+    var fileMetadata = {
         'name': req.body.space.displayName,
-    };
-
-    drive.files.create({
+        'mimeType': 'application/vnd.google-apps.folder'
+      };
+      drive.files.create({
         resource: fileMetadata,
-    });
-
-    text = `Folder: "${req.body.space.displayName}" was created in google drive, good luck with the project!`;
+        fields: 'id'
+      }, function (err, file) {
+        if (err) {
+          // Handle error
+            text = `Folder was not created!!!`;
+        } else {
+            text = `Folder: "${req.body.space.displayName}" with ID: "${file}" was created!`;
+        }
+      });
 
   // Case 2: When BOT was added to a DM
   } else if (req.body.type === 'ADDED_TO_SPACE' &&
