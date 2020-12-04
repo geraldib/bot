07bbf5d2477f380a638a3dc6f67b15f5789a42d2
Gerald Ibra <gerald.ibra@crispybacon.al>
2020-12-04 16:40:15 +0100

    dsad

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const PORT = process.env.PORT || 5000;
+const fs = require('fs');
+const readline = require('readline');
 const {google} = require('googleapis');
 
-const drive = 'https://www.googleapis.com/upload/drive/v3/files';
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = 'token.json';
 
 const app = express()
     .use(express.urlencoded({extended: false}))
@@ -16,32 +23,15 @@ app.get('/', (req, res) => {
     res.send('Hi');
 });
 
+
+
 app.post('/', (req, res) => {
 
   let text = '';
 
   // Case 1: When BOT was added to the ROOM
   if (req.body.type === 'ADDED_TO_SPACE' && req.body.space.type === 'ROOM') {
-    text = `Thanks for adding me to ${req.body.space.displayName}`;
-
-      const fileMetadata = {
-        'name': `${req.body.space.displayName}`,
-        'mimeType': 'application/vnd.google-apps.folder'
-      };
-
-      drive.files.create({
-        resource: fileMetadata,
-        fields: 'id'
-      }, function (err, file) {
-        if (err) {
-          // Handle error
-          console.error(err);
-        } else {
-            console.log("Created Ok");
-        }
-      });
-
-    text = `Folder: "${req.body.space.displayName}" was created!`;
+    text = `Thanks for adding me to the AAAA ${req.body.space.displayName}`;
 
 //   Case 2: When BOT was added to a DM
   } else if (req.body.type === 'ADDED_TO_SPACE' &&
@@ -51,6 +41,70 @@ app.post('/', (req, res) => {
   // Case 3: Texting the BOT
   } else if (req.body.type === 'MESSAGE') {
     text = `Your message was : ${req.body.message.text}`;
+
+        fs.readFile('credentials.json', (err, content) => {
+            if (err) return console.log('Error loading client secret file:', err);
+            // Authorize a client with credentials, then call the Google Drive API.
+            authorize(JSON.parse(content), listFiles);
+        });
+
+        function authorize(credentials, callback) {
+            const {client_secret, client_id, redirect_uris} = credentials.web;
+            const oAuth2Client = new google.auth.OAuth2(
+                client_id, client_secret, redirect_uris[0]);
+        
+            // Check if we have previously stored a token.
+            fs.readFile(TOKEN_PATH, (err, token) => {
+            if (err) return getAccessToken(oAuth2Client, callback);
+            oAuth2Client.setCredentials(JSON.parse(token));
+            callback(oAuth2Client);
+            });
+        }
+        
+        function getAccessToken(oAuth2Client, callback) {
+            const authUrl = oAuth2Client.generateAuthUrl({
+            access_type: 'offline',
+            scope: SCOPES,
+            });
+            console.log('Authorize this app by visiting this url:', authUrl);
+            const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+            });
+            rl.question('Enter the code from that page here: ', (code) => {
+            rl.close();
+            oAuth2Client.getToken(code, (err, token) => {
+                if (err) return console.error('Error retrieving access token', err);
+                oAuth2Client.setCredentials(token);
+                // Store the token to disk for later program executions
+                fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+                if (err) return console.error(err);
+                console.log('Token stored to', TOKEN_PATH);
+                });
+                callback(oAuth2Client);
+            });
+            });
+        }
+
+        function listFiles(auth) {
+            const drive = google.drive({version: 'v3', auth});
+            drive.files.list({
+            pageSize: 10,
+            fields: 'nextPageToken, files(id, name)',
+            }, (err, res) => {
+            if (err) return console.log('The API returned an error: ' + err);
+            const files = res.data.files;
+            if (files.length) {
+                console.log('Files:');
+                files.map((file) => {
+                console.log(`${file.name} (${file.id})`);
+                });
+            } else {
+                console.log('No files found.');
+            }
+            });
+        }
+
   }
 
   return res.json({text});
