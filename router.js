43409b1812b923cfb4f387dfd13e8156474d2b77
Gerald Ibra <gerald.ibra@crispybacon.al>
2020-12-07 12:38:56 +0100

    added route to get code

--- router.js
+++ router.js
@@ -1,6 +1,11 @@
 var express = require('express')
 var app = express()
 
+
+app.get('/', function (req, res) {
+    res.send(req.params)
+})
+
 app.get('/:code/:scope', function (req, res) {
     res.send(req.params)
 })
