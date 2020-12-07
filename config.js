857468759017920ecf704219ea872c7c202fe7db
Gerald Ibra <gerald.ibra@crispybacon.al>
2020-12-07 16:42:00 +0100

    dsa

---
+++ config.js
@@ -0,0 +1,25 @@
+const port = 3000;
+const baseURL = `https://gchatbotal.herokuapp.com`;
+
+module.exports = {
+  // The secret for the encryption of the jsonwebtoken
+  JWTsecret: 'geraldib',
+  baseURL: baseURL,
+  port: port,
+
+  // The credentials and information for OAuth2
+  oauth2Credentials: {
+    client_id: "512739329088-je70lfi9587ajq31gt5lepmvl0d6e868.apps.googleusercontent.com",
+    project_id: "quickstart-1607330313069", // The name of your project
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_secret: "UXtWJst8JNrfINCAL0t132X_",
+    redirect_uris: [
+        `${baseURL}/auth_callback`
+    ],
+    scopes: [
+      'https://www.googleapis.com/auth/drive.readonly'
+    ]
+  }
+};
---
+++ main.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const google = require('googleapis').google;
+const jwt = require('jsonwebtoken');
+// Google's OAuth2 client
+const OAuth2 = google.auth.OAuth2;
+// Including our config file
+const CONFIG = require('./config');
+// Creating our express application
+const app = express();
+// Allowing ourselves to use cookies
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+// Setting up EJS Views
+app.set('view engine', 'ejs');
+app.set('views', __dirname);
+// Listen on the port defined in the config file
+
+app.listen(CONFIG.port, function () {
+  console.log(`Listening on port ${CONFIG.port}`);
+});
+
+app.get('/', function (req, res) {
+
+    // Create an OAuth2 client object from the credentials in our config file
+    const oauth2Client = new OAuth2(
+        CONFIG.oauth2Credentials.client_id, 
+        CONFIG.oauth2Credentials.client_secret, 
+        CONFIG.oauth2Credentials.redirect_uris[0]
+    );
+
+    // Obtain the google login link to which we'll send our users to give us access
+    const loginLink = oauth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: CONFIG.oauth2Credentials.scopes
+    });
+
+    return res.render("index", { loginLink: loginLink });
+
+});
+
+app.get('/auth_callback', function (req, res) {
+    // Create an OAuth2 client object from the credentials in our config file
+    const oauth2Client = new OAuth2(
+        CONFIG.oauth2Credentials.client_id, 
+        CONFIG.oauth2Credentials.client_secret, 
+        CONFIG.oauth2Credentials.redirect_uris[0]
+    );
+    if (req.query.error) {
+      // The user did not give us permission.
+      return res.redirect('/');
+    } else {
+      oauth2Client.getToken(req.query.code, function(err, token) {
+        if (err)
+          return res.redirect('/');
+  
+        // Store the credentials given by google into a jsonwebtoken in a cookie called 'jwt'
+        // res.cookie('jwt', jwt.sign(token, CONFIG.JWTsecret));
+        // return res.redirect('/get_some_data');
+        return res.render("index", { code: req.query.code });
+      });
+    }
+  });
